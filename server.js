a7afb53e4a3736cdb95a8039b58b0a60c0b4b3da
dvavatsioulas <dvavatsioulas@gmail.com>
2019-11-07 11:31:38 +0200

    Add "/addproperty" endpoint

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 const express = require('express');
 var cors = require('cors');
 const mysql = require('mysql');
-//const fs = require('fs');
 const { WebhookClient } = require('dialogflow-fulfillment');
 
 const app = express();
 
 app.use(cors());
+
 //This is necessary for method post
 app.use(express.json());
-
-
 
 //Managing post requests from DialogFlow
 const dialogflowapp = require('./dialogflow/manage_d_post_req.js')
 dialogflowapp.dialogflow_post_requests(app, express, {WebhookClient})
 
+
 var con = mysql.createConnection({
-    host: "***REMOVED***", //or localhost
+    host: "***REMOVED***",
     port: 3306,
     user: "***REMOVED***",
     password: "***REMOVED***",
     database: "***REMOVED***"
 });
- 
+
 con.connect(function(err){
     if(err) throw err;
     console.log("Connected to DB");
@@ -45,24 +44,22 @@ app.get('/api/usertype',(req,res) => {
     }
 });
 
-/*
-IT DOESNT WORK
 //Create new item and add it in DB
-app.post('/api/properties',(req,res) => {
-    //let jsonData = require(req); a different way to read a json file i havent decide yet
-    fs.readFile(req, (err, data) => {
-        if (err) throw err;
-        let properties = JSON.parse(data);
-        con.query(`INSERT INTO properties VALUES (${properties.id},${properties.price},${properties.sqm},${properties.location},${properties.bedrooms},${properties.bathrooms},${properties.property_type},${properties.floor},${properties.description},${properties.sale_type},${properties.phone},${properties.email},${properties.img_url},${properties.furnitured},${properties.heating_type},${properties.built_year},${properties.parking}`, function (err, result, fields){
-            res.send(properties);
-        });
+//i should fix the null situation 
+//and if check if this house is duplicate
+app.post('/api/addproperty',(req,res) => {
+    var property = req.body;
+    con.query(`INSERT INTO properties (price,sqm,location,bedrooms,bathrooms,property_type,floor,description,sale_type,phone,email,img_url,furnitured,heating_type,built_year,parking)
+    VALUES (${property.price},${property.sqm},'${property.location}',${property.bedrooms},${property.bathrooms},${property.property_type},${property.floor},'${property.description}',${property.sale_type},'${property.phone}','${property.email}','${property.img_url}',${property.furnitured},'${property.heating_type}',${property.built_year},${property.parking})`, function (err, result, fields){
+        if(err) throw err;
+        res.send("Your house has been added successfully into DB!");
     });
-});
-*/
+    
+})
 
 //Returns the list of properties from the database
 app.get('/api/properties',(req,res) => {
-    con.query("SELECT id,price,sqm,location,bedrooms,bathrooms,floor,description,phone,email,img_url,furnitured,heating_type,built_year,parking,propertytypemapping.property_type_title,saletypemapping.sale_type_title FROM ((properties INNER JOIN propertytypemapping ON properties.property_type = propertytypemapping.property_type) INNER JOIN saletypemapping ON properties.sale_type =saletypemapping.sale_type);", function (err, result, fields) {
+    con.query("SELECT id,price,sqm,location,bedrooms,bathrooms,floor,description,phone,email,img_url,furnitured,heating_type,built_year,parking,propertytypemapping.property_type_title,saletypemapping.sale_type_title FROM ((properties INNER JOIN propertytypemapping ON properties.property_type = propertytypemapping.property_type) INNER JOIN saletypemapping ON properties.sale_type = saletypemapping.sale_type);", function (err, result, fields) {
       if (err) throw err;
       res.send(result);
     }); 
@@ -72,7 +69,7 @@ app.get('/api/properties',(req,res) => {
 
 //Returns the property with this ID
 app.get('/api/properties/:id',(req,res) => {
-    con.query(`SELECT id,price,sqm,location,bedrooms,bathrooms,floor,description,phone,email,img_url,furnitured,heating_type,built_year,parking,propertytypemapping.property_type_title,saletypemapping.sale_type_title FROM ((properties INNER JOIN propertytypemapping ON properties.property_type = propertytypemapping.property_type) INNER JOIN saletypemapping ON properties.sale_type =saletypemapping.sale_type) WHERE id=${parseInt(req.params.id)}`, function (err, result, fields) {
+    con.query(`SELECT id,price,sqm,location,bedrooms,bathrooms,floor,description,phone,email,img_url,furnitured,heating_type,built_year,parking,propertytypemapping.property_type_title,saletypemapping.sale_type_title FROM ((properties INNER JOIN propertytypemapping ON properties.property_type = propertytypemapping.property_type) INNER JOIN saletypemapping ON properties.sale_type = saletypemapping.sale_type) WHERE id=${parseInt(req.params.id)}`, function (err, result, fields) {
         if (err) throw err;
         if(isEmptyObject(result)) return res.status(404).send('There is no property with this id');
         res.send(result);
