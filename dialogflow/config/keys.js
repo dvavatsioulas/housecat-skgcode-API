5ff659971ddc5c5378872d9327c78fb81891a3da
dvavatsioulas <dvavatsioulas@gmail.com>
2019-11-18 15:03:32 +0200

    Add console messages for heroku testing

--- dialogflow/config/keys.js
+++ dialogflow/config/keys.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV==='production'){
     module.exports=require('./prod.js');
+	console.log("env: production");
 }else{
     module.exports = require('./dev.js')
+	console.log("env: development");
 }
