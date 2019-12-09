bbef5a6c483d0d2578c46385cb814ce47e84a30c
dvavatsioulas <dvavatsioulas@gmail.com>
2019-12-09 14:57:27 +0200

    Change DB credentials to environment variables

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ dialogflowapp.dialogflow_post_requests(app, express, {WebhookClient, Card, Sugge
 
 
 const pool = mysql.createPool({
-    host: "***REMOVED***",
-    port: 3306,
-    user: "***REMOVED***",
-    password: "***REMOVED***",
-    database: "***REMOVED***"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 app.get('/',(req,res) => {
