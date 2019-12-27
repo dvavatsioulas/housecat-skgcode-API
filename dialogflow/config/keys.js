09bc105f0b926686d6e4c48776361ca98583ab5c
Dimitris Vavatsioulas <dvavatsioulas@gmail.com>
2019-12-27 17:57:37 +0200

    Hide secret credentials

--- dialogflow/config/keys.js
+++ dialogflow/config/keys.js
@@ -1,7 +1,7 @@
-//if (process.env.NODE_ENV==='production'){
-    //module.exports=require('./prod.js');
-
-//}else{
+if (process.env.NODE_ENV==='production'){
+    module.exports=require('./prod.js');
+    console.log("env: production")
+}else{
     module.exports = require('./dev.js')
     console.log("env: dev")
-//}
+}
